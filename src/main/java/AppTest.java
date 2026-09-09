f522575feaaee172bcf6e1ea7cf5ad5632aa4fd3
technica123 <er.megha006@gmail.com>
2026-09-09 18:33:06 +0530

    Create AppTest.java

---
+++ src/main/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
