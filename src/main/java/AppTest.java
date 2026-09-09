4a009c28e4eed82079b4aade763f6f5363df5f96
technica123 <er.megha006@gmail.com>
2026-09-09 20:59:55 +0530

    Update AppTest.java

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
 
     @Test
-    void testAddition() {
+    public void testAddition() {
         assertEquals(5, App.add(2, 3));
     }
 }
