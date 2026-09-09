b66cfacd95a5f182aad2646ad61ac6c4fe376d00
technica123 <er.megha006@gmail.com>
2026-09-09 18:32:22 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps Ninja");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
